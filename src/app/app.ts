d0063c6a0624067ef8dd5ad041b1a2b32ffb2503
Linh Tran <lttran2003@gmail.com>
2025-11-15 17:16:00 -0700

    deleted comment

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,26 +1,3 @@
-// import { Component, signal, inject } from '@angular/core';
-// import { RouterOutlet } from '@angular/router';
-// import { Navbar } from './navbar/navbar';
-// import { Frame } from './frame/frame';
-// import { TranslateService, TranslatePipe, TranslateDirective } from "@ngx-translate/core";
-
-// @Component({
-//   selector: 'app-root',
-//   standalone: true,
-//   imports: [RouterOutlet, Navbar, Frame],
-//   templateUrl: './app.html',
-//   styleUrls: ['./app.scss']
-// })
-// export class App {
-//   protected readonly title = signal('Ozone Interactive Story');
-//   constructor(private translate: TranslateService) {
-//     // Load saved language from localStorage
-//     const savedLang = localStorage.getItem('lang') || 'en';
-//     translate.setDefaultLang('en');   // default fallback
-//     translate.use(savedLang);         // use persisted or default
-//   }
-// }
-
 import { Component, signal, Inject, PLATFORM_ID } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Navbar } from './navbar/navbar';
