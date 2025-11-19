39d3d092e6488174c747890c185a204c3ab8b4a2
logan-bird1 <lobi9969@colorado.edu>
2025-11-19 16:08:51 -0700

    button visibility implemented

--- src/app/navbar/navbar.ts
+++ src/app/navbar/navbar.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { selectedFrame } from '../frame/frame.state';
 import { TranslateService, TranslateModule } from "@ngx-translate/core";
+import { frameUnlocked } from '../frame/frame.state';
 
 @Component({
   selector: 'app-navbar',
@@ -10,7 +11,7 @@ import { TranslateService, TranslateModule } from "@ngx-translate/core";
   imports: [CommonModule, TranslateModule],
   templateUrl: './navbar.html',
   styleUrls: ['./navbar.scss'],
-  providers: [TranslateService]
+  providers: [TranslateService],
 })
 export class Navbar {
   readonly selectedFrame = selectedFrame;
@@ -18,6 +19,10 @@ export class Navbar {
 
   toggle() {
     this.open = !this.open;
+  }
+
+  buttonVisible(n: number) {
+    return frameUnlocked()[n];
   }
 
   go(n: number) {
