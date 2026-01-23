0ea055290372f493cdbe30014d361ca35cd4e09d
Linh Tran <lttran2003@gmail.com>
2026-01-22 23:19:12 -0700

    renamed background component to layer-wrapper

--- src/app/features/story-player/scenes/scene-sunny-day/scene-sunny-day.ts
+++ src/app/features/story-player/scenes/scene-sunny-day/scene-sunny-day.ts
@@ -2,13 +2,13 @@ import { Component, inject } from '@angular/core';
 import { StoryService } from '@core/story.service';
 import { TranslateModule } from '@ngx-translate/core';
 import { NarrativeText } from '@shared/ui/narrative-text/narrative-text';
-import { BackgroundComponent } from '@features/story-player/backgrounds/background/background.component';
+import { LayerWrapper } from '@features/story-player/layer-wrapper/layer-wrapper.component';
 import { SkyGrassComponent } from '@features/story-player/backgrounds/sky-grass/sky-grass.component';
 import { Clouds } from '@features/story-player/foregrounds/clouds/clouds';
 
 @Component({
   selector: 'app-scene-sunny-day',
-  imports: [NarrativeText, TranslateModule, BackgroundComponent, SkyGrassComponent, Clouds],
+  imports: [NarrativeText, TranslateModule, LayerWrapper, SkyGrassComponent, Clouds],
   templateUrl: './scene-sunny-day.html',
   styleUrl: './scene-sunny-day.scss',
 })
