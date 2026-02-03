160c977f4abe6a6d645313881d6e67b31fbfee71
NanoNewt <quinn@nicodemus.name>
2026-02-03 13:57:00 -0700

    added some more molecules and clouds

--- src/app/features/story-player/scenes/scene-gather-ingredients/scene-gather-ingredients.ts
+++ src/app/features/story-player/scenes/scene-gather-ingredients/scene-gather-ingredients.ts
@@ -3,10 +3,11 @@ import { StoryService } from '@core/story.service';
 import { TranslateModule } from '@ngx-translate/core';
 import { NarrativeText } from '@shared/ui/narrative-text/narrative-text';
 import { CdkDrag } from '@angular/cdk/drag-drop';
+import { Clouds } from '@features/story-player/foregrounds/clouds/clouds';
 const sceneName = 'scene-gather-ingredients';
 @Component({
   selector: 'app-' + sceneName,
-  imports: [NarrativeText, TranslateModule, CdkDrag],
+  imports: [NarrativeText, TranslateModule, CdkDrag, Clouds],
   templateUrl: './' + sceneName + '.html',
   styleUrl: './' + sceneName + '.scss',
 })
