aafb5b114e91a32d4f64535b1962a6245676e3cf
Matt Harper <matt.harper3415@gmail.com>
2026-02-06 17:16:01 -0700

    add wrapper, clean up

--- src/app/features/story-player/scenes/scene-burning-fuels/scene-burning-fuels.ts
+++ src/app/features/story-player/scenes/scene-burning-fuels/scene-burning-fuels.ts
@@ -1,13 +1,15 @@
 import { Component, inject, signal } from '@angular/core';
 import { StoryService } from '@core/story.service';
+import { SkyGrassComponent } from '@features/story-player/backgrounds/sky-grass/sky-grass.component';
+import { LayerWrapper } from '@features/story-player/layer-wrapper/layer-wrapper.component';
 import { TranslateModule } from '@ngx-translate/core';
 import { NarrativeText } from '@shared/ui/narrative-text/narrative-text';
-const sceneName = 'scene-burning-fuels';
+
 @Component({
-  selector: 'app-' + sceneName,
-  imports: [NarrativeText, TranslateModule],
-  templateUrl: './' + sceneName + '.html',
-  styleUrl: './' + sceneName + '.scss',
+  selector: 'app-scene-burning-fuels',
+  imports: [NarrativeText, TranslateModule, SkyGrassComponent, LayerWrapper],
+  templateUrl: './scene-burning-fuels.html',
+  styleUrl: './scene-burning-fuels.scss',
 })
 export class SceneBurningFuels {
   story = inject(StoryService);
