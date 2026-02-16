1ff2f18f2a0cc7cf3b85ca72bfd450dae1df298e
NanoNewt <quinn@nicodemus.name>
2026-02-16 15:07:41 -0700

    only drop in cloud
    made it so you can only drop an molecule in the ozone cloud

--- src/app/features/story-player/scenes/scene-gather-ingredients/scene-gather-ingredients.ts
+++ src/app/features/story-player/scenes/scene-gather-ingredients/scene-gather-ingredients.ts
@@ -2,7 +2,15 @@ import { Component, inject } from '@angular/core';
 import { StoryService } from '@core/story.service';
 import { TranslateModule } from '@ngx-translate/core';
 import { NarrativeText } from '@shared/ui/narrative-text/narrative-text';
-import { CdkDrag, CdkDragDrop, CdkDropList, transferArrayItem } from '@angular/cdk/drag-drop';
+import {
+  CdkDrag,
+  CdkDragDrop,
+  CdkDropList,
+  transferArrayItem,
+  Point,
+  DragRef,
+  CdkDragEnd,
+} from '@angular/cdk/drag-drop';
 const sceneName = 'scene-gather-ingredients';
 @Component({
   selector: 'app-' + sceneName,
@@ -10,11 +18,32 @@ const sceneName = 'scene-gather-ingredients';
   templateUrl: './' + sceneName + '.html',
   styleUrl: './' + sceneName + '.scss',
 })
+// class CdkDropBlog extends CdkDropList {
+//     constructor(){
+//         super()
+
+//     }
+
+// }
 export class SceneGatherIngredients {
   story = inject(StoryService);
 
   molecules1 = ['1', '2', '3', '4'];
-  smogParticals = ['5'];
+  ozoneCloudContents = ['5'];
+  travelingFromOzoneCloud = [];
+  travelingFromStartingZone = [];
+  handleDragEnd(event: CdkDragEnd) {
+    const position = event.dropPoint;
+    let inOzoneCloud = false;
+    const ozoneCloud = document.getElementById('ozone-cloud');
+    event.source.getRootElement().style.visibility = 'hidden';
+    const elementUnderItem = document.elementFromPoint(position.x, position.y);
+    event.source.getRootElement().style.visibility = '';
+    if (elementUnderItem !== ozoneCloud) {
+      // move back to previous drop container
+      event.source.reset();
+    }
+  }
   drop(event: CdkDragDrop<any>) {
     if (event.previousContainer !== event.container) {
       transferArrayItem(
