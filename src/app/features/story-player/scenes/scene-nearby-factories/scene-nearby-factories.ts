5571131ace99e123fbfaf689594967985fed0b3a
cewheeler24 <c.wheeler0824@gmail.com>
2026-03-08 14:27:21 -0600

    Slide 4 interaction completed

--- src/app/features/story-player/scenes/scene-nearby-factories/scene-nearby-factories.ts
+++ src/app/features/story-player/scenes/scene-nearby-factories/scene-nearby-factories.ts
@@ -1,14 +1,36 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { NgIf } from '@angular/common';
 import { StoryService } from '@core/story.service';
 import { TranslateModule } from '@ngx-translate/core';
 import { NarrativeText } from '@shared/ui/narrative-text/narrative-text';
 
 @Component({
   selector: 'app-scene-nearby-factories',
-  imports: [NarrativeText, TranslateModule],
+  imports: [NgIf, NarrativeText, TranslateModule],
   templateUrl: './scene-nearby-factories.html',
   styleUrl: './scene-nearby-factories.scss',
 })
 export class SceneNearbyFactories {
   story = inject(StoryService);
+
+  showFuel = signal(true);
+  fuelClicked = signal(false);
+  showOutput = signal(false);
+  outputMoved = signal(false);
+
+  clickFuel() {
+    if (!this.fuelClicked()) {
+      this.fuelClicked.set(true);
+      // hide fuel after animation completes
+      setTimeout(() => {
+        this.showFuel.set(false);
+        // spawn electricity and move it right
+        this.showOutput.set(true);
+        // mark scene complete when output is shown
+        this.story.setSceneCompleted(true);
+        setTimeout(() => this.outputMoved.set(true), 50);
+
+      }, 1000);
+    }
+  }
 }
