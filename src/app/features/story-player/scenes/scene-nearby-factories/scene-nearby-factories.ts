b0fd85d563021f986ae491feca6a225c1376e114
cewheeler24 <c.wheeler0824@gmail.com>
2026-03-08 16:00:38 -0600

    removed ngif stuff

--- src/app/features/story-player/scenes/scene-nearby-factories/scene-nearby-factories.ts
+++ src/app/features/story-player/scenes/scene-nearby-factories/scene-nearby-factories.ts
@@ -1,12 +1,11 @@
 import { Component, inject, signal } from '@angular/core';
-import { NgIf } from '@angular/common';
 import { StoryService } from '@core/story.service';
 import { TranslateModule } from '@ngx-translate/core';
 import { NarrativeText } from '@shared/ui/narrative-text/narrative-text';
 
 @Component({
   selector: 'app-scene-nearby-factories',
-  imports: [NgIf, NarrativeText, TranslateModule],
+  imports: [NarrativeText, TranslateModule],
   templateUrl: './scene-nearby-factories.html',
   styleUrl: './scene-nearby-factories.scss',
 })
