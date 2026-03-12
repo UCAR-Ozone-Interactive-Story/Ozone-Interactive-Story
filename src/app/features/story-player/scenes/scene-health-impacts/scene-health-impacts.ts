3c4aa1722d3871f911f2124279533a083cce23dc
Linh Tran <lttran2003@gmail.com>
2026-03-11 21:59:14 -0600

    accessibility and toggle button

--- src/app/features/story-player/scenes/scene-health-impacts/scene-health-impacts.ts
+++ src/app/features/story-player/scenes/scene-health-impacts/scene-health-impacts.ts
@@ -5,21 +5,35 @@ import { TranslateModule } from '@ngx-translate/core';
 import { NarrativeText } from '@shared/ui/narrative-text/narrative-text';
 import { LayerWrapper } from '@features/story-player/layer-wrapper/layer-wrapper.component';
 import { SkyGrassComponent } from '@features/story-player/backgrounds/sky-grass/sky-grass.component';
+import { MatButtonToggleModule, MatButtonToggleChange } from '@angular/material/button-toggle';
 
 @Component({
-  selector: 'app-scene-health-impacts',
-  imports: [NarrativeText, TranslateModule, LayerWrapper, SkyGrassComponent, CommonModule],
-  templateUrl: './scene-health-impacts.html',
-  styleUrl: './scene-health-impacts.scss',
+    selector: 'app-scene-health-impacts',
+    imports: [NarrativeText, TranslateModule, LayerWrapper, SkyGrassComponent, CommonModule, MatButtonToggleModule],
+    templateUrl: './scene-health-impacts.html',
+    styleUrl: './scene-health-impacts.scss',
 })
 export class SceneHealthImpacts {
     story = inject(StoryService);
-
     stage = signal(0);
+    pollutionChoice = "low";
 
-    advanceTime() {
-        if (this.stage() === 0) {
+    changeAirPollution(event: Event) {
+        const button = event.target as HTMLButtonElement;
+        const value = button.value;
+
+        const lowButton = document.getElementById('low-button');
+        const highButton = document.getElementById('high-button');
+
+        if (value === 'low') {
+            this.stage.set(0);
+            lowButton?.classList.add('selected');
+            highButton?.classList.remove('selected');
+        } else {
             this.stage.set(1);
+            highButton?.classList.add('selected');
+            lowButton?.classList.remove('selected');
         }
+
     }
 }
