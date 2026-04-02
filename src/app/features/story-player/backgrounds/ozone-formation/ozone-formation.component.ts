9e8cb225c6823a72caa0cececdad855dd7da3880
logan-bird1 <lobi9969@colorado.edu>
2026-04-01 21:12:17 -0600

    ozone-formation tweaks

---
+++ src/app/features/story-player/backgrounds/ozone-formation/ozone-formation.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-ozone-formation',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './ozone-formation.component.html',
+  styleUrls: ['./ozone-formation.component.scss'],
+})
+export class OzoneFormationComponent {}
