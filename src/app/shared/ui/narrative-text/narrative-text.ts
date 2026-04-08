228f66b74b7a7e0c0b0584bd6a5dfb5943438fc1
NanoNewt <quinn@nicodemus.name>
2026-04-08 09:49:15 -0600

    removed unneeded import

--- src/app/shared/ui/narrative-text/narrative-text.ts
+++ src/app/shared/ui/narrative-text/narrative-text.ts
@@ -6,7 +6,6 @@ import { setTabIndexOne } from './setTabIndexOne';
   standalone: true,
   templateUrl: './narrative-text.html',
   styleUrl: './narrative-text.scss',
-  imports: [setTabIndexOne],
 })
 export class NarrativeText implements OnDestroy {
   text = input.required<string>();
