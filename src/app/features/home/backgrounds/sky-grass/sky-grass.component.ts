73929c9b7d98fbcd10a6f2d9c3cf10a1c1437606
ty3thousand <tyca1249@colorado.edu>
2026-04-12 17:21:41 -0600

    Updates to home page

---
+++ src/app/features/home/backgrounds/sky-grass/sky-grass.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-sky-grass',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './sky-grass.component.html',
+  styleUrls: ['./sky-grass.component.scss'],
+})
+export class SkyGrassComponent {}
--- src/app/features/home/home.ts
+++ src/app/features/home/home.ts
@@ -1,11 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { StoryService } from '@core/story.service';
 import { TranslateModule } from '@ngx-translate/core';
 import { RouterModule } from '@angular/router';
 import { LanguageDropdownComponent } from './language-dropdown/language-dropdown';
+import { LayerWrapper } from '@features/story-player/layer-wrapper/layer-wrapper.component';
+import { SkyGrassComponent } from '@features/story-player/backgrounds/sky-grass/sky-grass.component';
+import { Clouds } from '@features/story-player/foregrounds/clouds/clouds';
 
+const sceneName = 'scene-end';
 @Component({
   selector: 'app-home',
-  imports: [LanguageDropdownComponent, TranslateModule, RouterModule],
+  imports: [LanguageDropdownComponent, TranslateModule, RouterModule, LayerWrapper, SkyGrassComponent, Clouds],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
